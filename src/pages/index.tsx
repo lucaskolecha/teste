622de97e0a1d05c95bb7177861e0e4ffc2791b67
Lucas <lskolecha@gmail.com>
2021-08-12 10:36:00 -0300

    Correct

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,16 @@ import axios from 'axios';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+
 import Page from '../components/common/Page';
 
 const Home: React.FC = () => {
   const [repositories, setRepositories] = useState<any[]>([]);
   const [text, setText] = useState('');
   useEffect(() => {
-    axios
-      .get(`https://w3rakeqchc.execute-api.us-east-2.amazonaws.com/dev/meals`)
-      .then((response) => {
-        console.log(response.data);
-        setRepositories(response.data);
-      });
+    axios.get('https://ana-personal-project.herokuapp.com/meal').then((response) => {
+      setRepositories(response.data[0].meals);
+    });
   }, []);
 
   return (
@@ -21,18 +19,22 @@ const Home: React.FC = () => {
       <Page title="Página Principal">
         <div className="flex items-center justify-center flex-col h-full overflow-auto">
           <h1 className="text-xl my-7">What's on your mind?</h1>
-          <div className="grid grid-cols-4 grid-rows-4 gap-8 w-2/4 mt-8">
+          <div className="conteudo">
             {repositories.map((repo) => (
-              <div className="flex items-center justify-center flex-col mt-8" key={repo.idMeal}>
-                <img src={repo.strMealThumb} alt={repo.strMeal} width="100"></img>
-                <p className="text-center mt-4">
-                  <span className="font-bold">Descrição:</span>
-                  <br /> Teste descrição
+              <div className="flex items-center justify-center flex-col mt-10" key={repo.idMeal}>
+                <h2 className="text-left mt-8">
+                  <br /> {repo.strMeal}
+                </h2>
+                <img
+                  className=".imagem"
+                  src={repo.strMealThumb}
+                  alt={repo.strMeal}
+                  width="200"
+                ></img>
+                <p className="conteudo-right">
+                  <br /> {repo.strInstructions}
                 </p>
-                <p className="text-center mt-4">
-                  <span className="font-bold">Modo de preparo:</span>
-                  <br /> Teste Modo de preparo
-                </p>
+                <hr></hr>
               </div>
             ))}
           </div>
